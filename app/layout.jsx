07fe5103fe78c780fadb291729642a28cc67e4b2
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-05-09 14:43:58 +0600

    create post added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,32 +1,27 @@
 import Nav from '@components/Nav';
 import Provider from '@components/Provider';
 import '@styles/global.css';
-import Head from './head';
-import Home from './page';
 
 export const metadata = {
   title: 'AI-Flow',
   description: 'Discover & Share AI Prompts',
 };
 
-const Layout = ({ Children }) => {
-  return (
-    <html lang="en">
-      <body>
-        <Provider>
-          <div className="main">
-            <div className="gradient" />
-          </div>
-          <main className="app">
-            <Head />
-            <Nav />
-            <Home />
-            {Children}
-          </main>
-        </Provider>
-      </body>
-    </html>
-  );
-};
+const RootLayout = ({ children }) => (
+  <html lang="en">
+    <body>
+      <Provider>
+        <div className="main">
+          <div className="gradient" />
+        </div>
 
-export default Layout;
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </Provider>
+    </body>
+  </html>
+);
+
+export default RootLayout;
