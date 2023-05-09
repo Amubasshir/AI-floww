f6135cdd2b4479603cfb4e0d2c179f478220e38e
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-05-09 20:47:23 +0600

    create post done

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,5 +1,51 @@
+'use client';
+
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+
+const { default: Form } = require('@components/Form');
+
 const CreatePrompt = () => {
-  return <div>CreatePrompt</div>;
+  const Router = useRouter();
+  const { data: session } = useSession();
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: '',
+    tag: '',
+  });
+
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+    try {
+      const response = await fetch('/api/prompt/new', {
+        method: 'POST',
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+      if (response.ok) {
+        Router.push('/');
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  return (
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  );
 };
 
 export default CreatePrompt;
